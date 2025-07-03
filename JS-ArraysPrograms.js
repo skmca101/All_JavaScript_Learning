1623178998c45e5b8c56670181465b3b1c4b2696
Santosh Kumar <131803097+skmca101@users.noreply.github.com>
2025-07-03 20:23:44 +0530

    Update JS-ArraysPrograms.js

--- JS-ArraysPrograms.js
+++ JS-ArraysPrograms.js
@@ -50,6 +50,43 @@ let result = zero.concat(nonZero)
 console.log(result)
 //==============================================================
 
+let arr = [7,3,5,4,2,1,8]
+// let arrLength = arr.length
+// console.log(arrLength)
+//======Add in begin/end of array=============
+// arr.push(6)
+// arr.unshift(0)
+// console.log(arr)
+//======Remove in begin/end of array=============
+// arr.pop(8)
+// arr.shift(7)
+//arr.splice(1,0,6)
+//let arr1 = arr.reverse()
+//console.log(arr1)
+// let sortedArr = arr.sort((a,b)=>a-b)
+// console.log(sortedArr)
+//console.log(arr.includes(1))
+let frequency = {};
+let even = 0;
+let odd = 0;
+for(let i = 0; i < arr.length; i++)
+{
+    let ele = arr[i]
+     if(ele % 2 == 0)
+    {
+        even++
+      
+    }
+    else
+    {
+        odd++
+    }
+}
+
+
+
+  console.log("Total even no. in array",even)
+  console.log("Total odd no. in array", odd)
 
 
 
