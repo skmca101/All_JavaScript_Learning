204e46491acdb3c8e91d544205378ff91c1fee4c
Santosh Kumar <131803097+skmca101@users.noreply.github.com>
2025-07-04 17:58:35 +0530

    Update JS-ArraysPrograms.js

--- JS-ArraysPrograms.js
+++ JS-ArraysPrograms.js
@@ -60,7 +60,9 @@ let arr = [7,3,5,4,2,1,8]
 //======Remove in begin/end of array=============
 // arr.pop(8)
 // arr.shift(7)
+//=======Add element in a specific position==========
 //arr.splice(1,0,6)
+//================Reverse an array===================
 //let arr1 = arr.reverse()
 //console.log(arr1)
 // let sortedArr = arr.sort((a,b)=>a-b)
@@ -88,6 +90,42 @@ for(let i = 0; i < arr.length; i++)
   console.log("Total even no. in array",even)
   console.log("Total odd no. in array", odd)
 
+//=============================================
+// let arr = [1,4,6,2,8,3,9,0]
+// let arr1 = Math.max(...arr)
+// let arr2 = Math.min(...arr)
+// console.log(arr1)
+// console.log(arr2)
+// //=========Second Method using for loop==========
+// const numbers = [3, 1, 4, 1, 5, 9, 2, 6];
+// let maxNumber = numbers[0]; // Initialize with the first element
 
+// for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > maxNumber) {
+//             maxNumber = numbers[i];
+//         }
+// }
+
+// console.log(maxNumber); // Output: 9
+
+//==========Sum of all elements================
+try{
+   let numbers1 = [3, 1, 4, 1, 5, 9, 2, 6];
+let sum = 0;
+for(let i = 0; i < numbers1.length; i++)
+{
+    sum = sum + numbers1[i]
+}
+console.log(sum) 
+let arr = [...new Set(numbers1)]
+console.log(arr)
+
+let sorted = arr.sort((a, b) => a-b)
+console.log(sorted)
+}
+catch(err)
+{
+    console.log([err.name, err.message])
+}
 
 
